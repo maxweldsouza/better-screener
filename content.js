7b7c960d007a30dc0c88bd0276c4c3aa60ce3d79
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2025-03-01 11:19:56 +0530

    feat: Works

--- content.js
+++ content.js
@@ -1,4 +1,8 @@
 (function() {
+    const MoreSelector = `#metrics-dropdown > button`
+    const salesAndMarginSelector = `button[value="GPM-OPM-NPM-Quarter Sales"]`
+    const PESelector = `button[value="Price to Earning-Median PE-EPS"]`
+    const MaxPeriodSelector = `button[value="10000"]`
     function clickButton(selector, delay = 0) {
         setTimeout(() => {
             var btn = document.querySelector(selector);
@@ -13,7 +17,8 @@
 
     // Wait for the page to fully load
     window.addEventListener("load", function() {
-        clickButton('button[value="10000"]');
-        clickButton('button[value="Price to Earning-Median PE-EPS"]');
+        if (!window.location.pathname.startsWith('/company')) return
+        clickButton(MoreSelector);
+        clickButton(salesAndMarginSelector, 100);
     });
 })();
