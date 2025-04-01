cd6383025822657b46c465fd0de24d4acd3d17e6
Maxwel Dsouza <2565045+maxweldsouza@users.noreply.github.com>
2025-04-01 17:03:51 +0530

    feat: Sales

--- content.js
+++ content.js
@@ -39,10 +39,10 @@
     function renderToolbar(container) {
         // Create a new element using template string
         const toolbar = `
-        <div class="flex">
+        <div class=" flex">
             <div class="options flex">
-                <button type="button" class="active">Net Profit</button>
-                <button type="button">Sales</button>
+                <button type="button" class="better-screener-button active">Net Profit</button>
+                <button type="button" class="better-screener-button">Sales</button>
             </div>
         </div>
     `;
@@ -50,6 +50,67 @@
         container.insertAdjacentHTML("beforeend", toolbar);
     }
 
+    function plotData(label, labels, data, topElement) {
+        // Create a canvas element dynamically
+        const canvas = document.createElement('canvas');
+
+        const container = document.createElement('div')
+        container.setAttribute('class', 'card card-large')
+        container.setAttribute('style', 'height:480px')
+
+        const header = document.createElement('h2')
+        header.innerText = label
+        container.appendChild(header)
+        container.appendChild(canvas)
+
+
+        topElement.insertAdjacentElement("afterend", container)
+        const ctx = canvas.getContext('2d');
+
+        let chart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: label,
+                    data: data,
+                    fill: true,
+                    backgroundColor: '#a39dfb'
+                }]
+            },
+            options: {
+                plugins: {
+                    legend: {
+                        display: false
+                    }
+                },
+                responsive: true,
+                maintainAspectRatio: false,
+                scales: {
+                    x: {
+                        grid: {
+                            display: false // Remove vertical gridlines
+                        }
+                    },
+                    y: {
+                        grid: {
+                            display: true // Keep horizontal gridlines
+                        },
+                        beginAtZero: true
+                    }
+                },
+                layout: {
+                    padding: {
+                        left: 30,
+                        right: 30,
+                        top: 0,
+                        bottom: 90
+                    }
+                },
+            }
+        });
+
+    }
     function getNetProfitDataAndPlot() {
         const table = document.querySelector("#profit-loss table.data-table");
         if (!table) {
@@ -59,75 +120,15 @@
         const labels = getLabels(table)
         const netProfitData = getTableRowData(table, "Net Profit")
         const salesData = getTableRowData(table, "Sales")
-        console.log("salesData", salesData);
 
         if (netProfitData.length === 0) {
             console.error("Net Profit row not found.");
             return;
         }
 
-        // Create a canvas element dynamically
-        const canvas = document.createElement('canvas');
-        canvas.setAttribute('id', 'better-screener-canvas')
-
-        const container = document.createElement('div')
-        container.setAttribute('class', 'card card-large')
-        container.setAttribute('style', 'height:640px')
-
-        renderToolbar(container)
-
-        container.appendChild(canvas)
-
-
         const topElement = document.getElementById("top");
-        topElement.insertAdjacentElement("afterend", container)
-        const ctx = canvas.getContext('2d');
-
-        let chart
-        // Create the chart using the preloaded Chart.js
-            chart = new Chart(ctx, {
-                type: 'bar',
-                data: {
-                    labels: labels,
-                    datasets: [{
-                        label: 'Net Profit (Rs. Crores)',
-                        data: netProfitData,
-                        fill: true,
-                        backgroundColor: '#a39dfb'
-                    }]
-                },
-                options: {
-                    plugins: {
-                        legent: {
-                            display: false
-                        }
-                    },
-                    responsive: true,
-                    maintainAspectRatio: false,
-                    scales: {
-                        x: {
-                            grid: {
-                                display: false // Remove vertical gridlines
-                            }
-                        },
-                        y: {
-                            grid: {
-                                display: true // Keep horizontal gridlines
-                            },
-                            beginAtZero: true
-                        }
-                    },
-                    layout: {
-                        padding: {
-                            left: 30,
-                            right: 30,
-                            top: 0,
-                            bottom: 60
-                        }
-                    },
-                }
-            });
-
+        plotData('Sales', labels, salesData, topElement)
+        plotData('Net Profit', labels, netProfitData, topElement)
     }
 
 
